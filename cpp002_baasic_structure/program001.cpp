78222d2569d80d2cdc87767fada4a2fde173de64
driyo <debashisr433@gmail.com>
2024-05-10 00:45:57 +0530

    .-

---
+++ cpp002_baasic_structure/program001.cpp
@@ -0,0 +1,14 @@
+#include<iostream>      // including a header file name = iostream 
+                        // iostream mainly considered for input and output processes
+
+int main(){             // int defines that the function will return a int value
+                        // main() is a function
+    std::cout<< "hello world";  // cout is a output function just like print in python
+    return 0;                   // here the return value is 0 for the function
+}
+
+// output :
+// PS C:\Users\lenovo\Documents\GitHub\cpp\cpp002_baasic_structure> g++ .\program001.cpp 
+// - here g++ is a compiler which compiles the cpp file into .exe
+// PS C:\Users\lenovo\Documents\GitHub\cpp\cpp002_baasic_structure> .\a.exe
+// hello world
