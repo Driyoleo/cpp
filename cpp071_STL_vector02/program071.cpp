c0c12bd58c6e95eeb6f935cd6f889f1e5d40efef
driyo <debashisr433@gmail.com>
2024-07-03 10:21:48 +0530

    vector : capacity member function

--- cpp071_STL_vector02/program071.cpp
+++ cpp071_STL_vector02/program071.cpp
@@ -14,6 +14,20 @@ int main()
     }
     cout << endl ;
     cout << "size of vector now is : " << vec.size() << endl ;
-    cout << "is the vector empty now ? (0/1) : " << vec.empty() << endl ; 
+    cout << "is the vector empty now ? (0/1) : " << vec.empty() << endl ;
+    cout << endl ;
+
+
+    vector <int> vec1 ;
+    cout << vec1.size() <<  " " << vec1.capacity() <<  endl ;
+    vec1.resize(10) ;
+    cout << vec1.size() << " " << vec1.capacity() << endl ;
+    vec1.resize(5) ;
+    cout << vec1.size() << " " << vec1.capacity() << endl ;
+    vec1.shrink_to_fit() ;
+    cout << vec1.size() << " " << vec1.capacity() << endl ;  
+
+
+
     return 0 ;
 }
